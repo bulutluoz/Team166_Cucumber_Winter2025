3503383cb37c4de30f531e2e3c289d1982b815f7
bulutluoz <abbulutluoz@gmail.com>
2025-11-01 20:37:49 +0100

    5.ders sonu

--- src/test/java/stepdefinitions/TestotomasyonuStepdefinitions.java
+++ src/test/java/stepdefinitions/TestotomasyonuStepdefinitions.java
@@ -1,26 +1,33 @@
 package stepdefinitions;
 
 import io.cucumber.java.en.*;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.Keys;
+import pages.TestotomasyonuPage;
+import utilities.ConfigReader;
+import utilities.Driver;
 
 public class TestotomasyonuStepdefinitions {
+    TestotomasyonuPage testotomasyonuPage = new TestotomasyonuPage();
 
     @Given("kullanici testotomasyonu anasayfaya gider")
     public void kullanici_testotomasyonu_anasayfaya_gider() {
-
+        Driver.getDriver().get(ConfigReader.getProperty("toUrl"));
     }
 
     @When("arama kutusuna phone yazip aratir")
     public void arama_kutusuna_phone_yazip_aratir() {
-
+        testotomasyonuPage.aramaKutusu.sendKeys("phone" + Keys.ENTER);
     }
 
     @Then("arama sonucunda urun bulunabildigini test eder")
     public void arama_sonucunda_urun_bulunabildigini_test_eder() {
-
+        int aramaSonucsayisi = testotomasyonuPage.bulunanUrunElementleriList.size();
+        Assertions.assertTrue(aramaSonucsayisi > 0);
     }
 
     @Then("sayfayi kapatir")
     public void sayfayi_kapatir() {
-
+        Driver.quitDriver();
     }
 }
